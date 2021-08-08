39056d9715280f64a30f9e1dee2bd21af5991d6b
Andres Gomez <anfegor@hotmail.com>
2021-08-08 01:30:58 -0500

    added first versions of shapes.html and shapes.js

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # curso-pratico-javascript
+
+...
+
+## Taller #1: Figuras Geométricas
+
+-Primer paso: Definir las formulas
+-Segundo paso: implementar formulas en JavaScrips
+-Tercer paso: crear funciones
+-Cuarto paso: Integrar JavaScript con HTML
---
+++ shapes.js
@@ -0,0 +1,32 @@
+
+//Code for the square
+console.group("squares")
+const squareSide = 5;
+console.log("the square sides measure: " + squareSide + "cm" + " each")
+
+const squarePerimeter = squareSide * 4;
+console.log("the square perimeter is: " + squarePerimeter + "cm.")
+
+const squareArea = squareSide * squareSide;
+console.log("the square area is: " + squareArea + "cm^2.");
+console.groupEnd()
+
+//code for triangle
+const triangleSide1 = 6;
+const triangleSide2 = 6;
+const triangleBase = 4;
+
+
+console.group("Triangles")
+console.log(
+    "the triangle sides measure: "
+     + triangleSide1
+     + "cm, "
+     + triangleSide2
+     + "cm, " + triangleBase
+     + "cm."
+    )
+
+const triangleHeight = 5.5;
+console.log("the triangle height is: " + triangleHeight + "cm.")
+
