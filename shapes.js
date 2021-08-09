b603c674182ce8133c8aab5949eef384b0f32a1d
Andres Gomez <anfegor@hotmail.com>
2021-08-09 00:49:58 -0500

    Created functions for each shape to be able to calculate from JS console

--- shapes.js
+++ shapes.js
@@ -1,32 +1,75 @@
 
 //Code for the square
 console.group("squares")
-const squareSide = 5;
-console.log("the square sides measure: " + squareSide + "cm" + " each")
+// const squareSide = 5;
+// console.log("the square sides measure: " + squareSide + "cm" + " each")
 
-const squarePerimeter = squareSide * 4;
-console.log("the square perimeter is: " + squarePerimeter + "cm.")
+function squarePerimeter(side) {
+    return side * 4;
+}
 
-const squareArea = squareSide * squareSide;
-console.log("the square area is: " + squareArea + "cm^2.");
+function squareArea(side) {
+    return side * side;
+}
 console.groupEnd()
 
 //code for triangle
-const triangleSide1 = 6;
-const triangleSide2 = 6;
-const triangleBase = 4;
-
+// const triangleSide1 = 6;
+// const triangleSide2 = 6;
+// const triangleBase = 4;
 
 console.group("Triangles")
-console.log(
-    "the triangle sides measure: "
-     + triangleSide1
-     + "cm, "
-     + triangleSide2
-     + "cm, " + triangleBase
-     + "cm."
-    )
+// console.log(
+//     "the triangle sides measure: "
+//      + triangleSide1
+//      + "cm, "
+//      + triangleSide2
+//      + "cm, "
+//      + triangleBase
+//      + "cm."
+//     )
 
-const triangleHeight = 5.5;
-console.log("the triangle height is: " + triangleHeight + "cm.")
+// const triangleHeight = 5.5;
+
+function trianglePerimeter(side1,side2,base) {
+    return side1 + side2 + base;
+}
+
+function triangleArea(base,height){
+    return (base * height) / 2;
+}
+
+console.groupEnd();
+
+// Code for circle
+console.group("Circles")
+
+// Radius
+// const circleRadius = 4;
+
+// Diameter
+function circleDiameter(radius){
+    return radius * 2
+}
+
+// PI
+
+const PI = Math.PI;
+
+//Perimeter
+function circlePerimeter(radius){
+    const diameter = circleDiameter(radius)
+    return diameter * PI;
+}
+
+// Radius
+function circleArea (radius) {
+    return (radius * radius) * PI
+}
+
+console.groupEnd()
+
+
+
+
 
